59a1846fbdb9dd712a87ccc093686af9ab734fcb
Nikolai Lønberg <niklonberg@gmail.com>
2023-10-05 15:54:21 +0200

    add references to form elements

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
 const log = console.log;
+
 const form = document.querySelector("#sign-up-form");
-log(form);
+
+const password = document.querySelector("#user-pass");
+const confirmPassword = document.querySelector("#confirm-pass");
+
+const submitBtn = document.querySelector("#submit-form-btn");
+
+/* form.addEventListener("submit", (event) => {
+  event.preventDefault();
+}); */
+
+submitBtn.addEventListener("click", (event) => {
+  log(event);
+});
