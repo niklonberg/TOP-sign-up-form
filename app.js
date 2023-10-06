b9d5cd3521a8f339b67b1546496a666c1c5871c6
Nikolai Lønberg <niklonberg@gmail.com>
2023-10-06 13:37:18 +0200

    move code from submitBtn event listener to form

--- app.js
+++ app.js
@@ -7,17 +7,23 @@ const confirmPassword = document.querySelector("#confirm-pass");
 
 const submitBtn = document.querySelector("#submit-form-btn");
 
-/* form.addEventListener("submit", (event) => {
+form.addEventListener("submit", (event) => {
   event.preventDefault();
-}); */
-
-submitBtn.addEventListener("click", (event) => {
   if (password.value === confirmPassword.value) {
     form.submit();
+    log("form submitted");
   } else {
-    event.preventDefault();
     password.classList.add("input-invalid");
     confirmPassword.classList.add("input-invalid");
     log("error");
   }
 });
+
+/* some kind of loop to add valid/invalid classes to inputs, when user is
+  finished with an input (blur?)
+  check if they have an isValid property or something,
+  can then apply valid/invalid-input class to show checkmark/error and 
+  hide / unhide descriptive labels telling how to fix the issue.
+*/
+
+/* submitBtn.addEventListener("click", (event) => {}); */
